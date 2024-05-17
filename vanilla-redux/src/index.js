332c2edd1fb31d29618fbe98b0459e2f7213020b
jsum01 <djadjawltjr@gmail.com>
2024-05-17 13:54:48 +0900

    Update Plus&Minus Feature
    Use
    - Reducer
    - Subscirbe
    - Dispatch

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,28 +1,46 @@
 import { createStore } from "redux";
-// store is input your data
-// state changing app's data 
 
+// DOM 요소 참조
 const add = document.getElementById("add");
-const minus = document.getElementById("minus")
+const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-number.innerText = count;
+// 2. Reducer: 리듀서 함수 생성
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
 
-const updateText = () => {
-  number.innerText = count;
-}
+// 1. CreateStore: 스토어 생성
+const countStore = createStore(countModifier);
 
+// 4. Subscribe: 스토어 변화를 감지하는 함수 정의
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+
+// 초기 상태 표시
+number.innerText = countStore.getState();
+
+// 3. 액션 디스패치 함수 정의
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
-}
+  countStore.dispatch({ type: MINUS });
+};
 
+// 버튼에 이벤트 리스너 추가
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
