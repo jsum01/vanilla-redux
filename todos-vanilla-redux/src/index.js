6367d780751c344dd1ad8a0f5e3729ba71d483ef
jsum01 <djadjawltjr@gmail.com>
2024-05-17 15:03:52 +0900

    Update dispatchDeleteTodo & Devide actions

--- todos-vanilla-redux/src/index.js
+++ todos-vanilla-redux/src/index.js
@@ -10,13 +10,27 @@ import { createStore } from "redux";
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+//보통 이렇게 action만을 리턴하는 것을 따로 만들어서 코드를 최적화한다. 그리고 이는 보통 reducer 위에 위치시킨다.
+/**
+ * 
+ * @param {*} text 
+ * @returns 
+ */
+const addTodo = (text) => {
+  return { type: ADD_TODO, text };
+}
+
+const deleteTodo = (id) => {
+  return { type: DELETE_TODO, id };
+}
+
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
       return [...state, { text: action.text, id: Date.now() }]; // state를 changin하는 것이 아니라 우리가 직접 mutate해서 return하는 것이다.
     case DELETE_TODO:
-      return [];
+      return state.filter(item => item.id !== parseInt(action.id, 10)); // filtering
     default:
       return state;
   }
@@ -24,13 +38,41 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer); // store은 read only기 때문에 action을 통해 수정해줘야 한다.
 
+// ------------------------------------Functions------------------------------------ //
+
+/**
+ * Add
+ * @param {*} text 
+ */
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text));
+}
+
+const dispatchDeleteTodo = (e) => {
+  // 어떤 버튼이 눌리고 있는지 알아야 함 -> e.target.parentNode
+  console.log(e.target.parentNode.id); // parentNode는 li이므로 li의 id를 출력
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+}
+
+const onSubmit = e => {
+  e.preventDefault();
+  const todo = input.value;
+  input.value = "";
+  dispatchAddTodo(todo);
+};
+
 const paintTodos = () => {
   const toDos = store.getState();
   ul.innerHTML = ""; // 이렇게 비워주지 않으면 repainting된다.
   toDos.forEach(todo => {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.addEventListener('click', dispatchDeleteTodo);
     li.id = todo.id;
-    li.innerText = todo.text;
+    li.innerText = todo.text + " ";
+    btn.innerText = "DELETE"
+    li.appendChild(btn); // 삭제할 수 있는 버튼도 같이 추가해준다.
     ul.appendChild(li);
   })
 }
@@ -39,20 +81,5 @@ store.subscribe(paintTodos);
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
-
-/**
- * Add
- * @param {*} text 
- */
-const addTodo = (text) => {
-  store.dispatch({ type: ADD_TODO, text });
-}
-
-const onSubmit = e => {
-  e.preventDefault();
-  const todo = input.value;
-  input.value = "";
-  addTodo(todo);
-};
 
 form.addEventListener("submit", onSubmit);
